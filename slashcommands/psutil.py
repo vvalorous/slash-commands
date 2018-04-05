13fcee566ce54fe73971a90fbffb7c324800b679
Akhil Lawrence <akhil.lawrence@lntinfotech.com>
2018-04-05 11:17:30 +0530

    updated command

--- slashcommands/psutil.py
+++ slashcommands/psutil.py
@@ -7,7 +7,7 @@ from .base import SlashCommand
 
 class PSUtil(SlashCommand):
     """ Command to perform tasks associated with processes """
-    command = 'psutil'
+    command = '/psutil'
 
     def handler(self):
         """ This method defines what needs to be done when this command is invoked  """
