5a609cef52c4d06d9be338f859511782ebfaff4f
Akhil Lawrence <akhil.lawrence@lntinfotech.com>
2018-04-07 23:55:45 +0530

    add systemctl

---
+++ slashcommands/systemctl.py
@@ -0,0 +1,16 @@
+# coding=utf-8
+
+import subprocess
+
+from .base import SlashCommand
+
+
+class Systemctl(SlashCommand):
+    """ Command to perform tasks associated with services """
+    command = '/systemctl'
+
+    def handler(self):
+        """ This method defines what needs to be done when this command is invoked  """
+        command = self.payload['text']
+        proc = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+        return {'text': '\n'.join([x for x in proc.stdout])}
