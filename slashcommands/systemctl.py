8c3cebb51c0f56b9f9b3396f16f5a559bee9aa45
Akhil Lawrence <akhil.lawrence@lntinfotech.com>
2018-04-08 00:01:27 +0530

    updated keyword for systemctl

--- slashcommands/systemctl.py
+++ slashcommands/systemctl.py
@@ -7,7 +7,7 @@ from .base import SlashCommand
 
 class Systemctl(SlashCommand):
     """ Command to perform tasks associated with services """
-    command = '/systemctl'
+    command = '/service'
 
     def handler(self):
         """ This method defines what needs to be done when this command is invoked  """
